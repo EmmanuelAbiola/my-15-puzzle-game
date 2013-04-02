690ef2511a9e2a671d6dfd843f63b8e684365ada
ola.horovits <ola.horovits@gmail.com>
2013-04-02 16:14:51 +0000

    Changing the logic of the design.Field[4][4] instead of the Numbers class. Adding logic for image movement,by finger touch.The landscape mode is "removed" to portrait mode only for the Gyro. purposes.

--- FifteenPuzzle/src/com/game/fifteenpuzzle/MainActivity.java
+++ FifteenPuzzle/src/com/game/fifteenpuzzle/MainActivity.java
@@ -1,56 +1,152 @@
 package com.game.fifteenpuzzle;
 
+
+import com.game.fifteenpuzzle.BmpSettings;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
-
-public class MainActivity extends Activity {
+import android.widget.Toast;
+import android.view.View.OnTouchListener;
+import android.content.Intent;
+public class MainActivity extends Activity implements OnTouchListener{//,SensorEventListener{
 
 	//private Numbers num;
+	private SensorManager sensorManager;
+	
+	static float x10,y10,z10;
+	static int num=0; 
+	boolean InitFlag=true;
+	static float initGyroX,initGyroY,initGyroZ;
+	
 	private Numbers[] numbers = new Numbers[16]; // array that holds the numbers
+	BmpSettings myBmp[] = new BmpSettings[15];
 	Context mContext;
 	Drawable d;
+	int x_pos=120,y_pos=240;
+	int x_InitPos=120,y_InitPos=240;
+	int x_InitPosLand=360,y_InitPosLand=50;
 	
+	gameField game15puzzle;
+	Display display = null;
+	
+	//default resolution of the screen
+	public static int defaultRes_X = 480;
+	public static int defaultRes_Y = 600;
+	
+	//current resolution of the screen
+	public static int currentWidth_X;
+	public static int currentHeight_Y;
+		
+	int[] mBmpArray = { R.drawable.num16blank,R.drawable.num1, R.drawable.num2,
+            R.drawable.num3, R.drawable.num4, R.drawable.num5,
+            R.drawable.num6, R.drawable.num7,
+            R.drawable.num8, R.drawable.num9, R.drawable.num10,
+            R.drawable.num11, R.drawable.num12,
+            R.drawable.num13, R.drawable.num14,R.drawable.num15
+	};
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+		protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		mContext = getApplicationContext();
-		 
-		for(int i=0;i<16;i++)
-		    	numbers[i] =  new Numbers(mContext,i);
+		game15puzzle = new gameField();
 		
+		setDefaultScreen(defaultRes_X,defaultRes_Y);
+		
+		//load the bmps
+		for (int i = 0; i < 15; i++) {
+			myBmp[i] = new BmpSettings(mContext.getResources(),mBmpArray[i]);
+		
+			
+	        // Retrieve the SensorManager. 
+	       // sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+	        //Retrieve the default Sensor for the gyroscope.
+	        //Sensor sensorGyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+	        //Register this activity as the listener for gyroscope events.
+	        //sensorManager.registerListener(this, sensorGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+			
 		/*
          * Create an instance of an anonymous class whose base class is
          * android.view.View and whose method onDraw() is overridden.
          */
         View rootView = new View(this) {
 
-            /*
-             * Method onDraw() will be called by Android whenever the view needs
-             * to be redrawn. It is given a Canvas object that can be used for
-             * drawing operations.
-             */
+        
             @Override
             protected void onDraw(Canvas canvas) {
                 super.onDraw(canvas);
-
-                for(int i=0;i<16;i++){ 
-                	d = getResources().getDrawable(numbers[i].getNumImg());
-                	d.setBounds(numbers[i].getCoordX(),numbers[i].getCoordY() ,numbers[i].getCoordX()+120, numbers[i].getCoordY()+120);
-                	d.draw(canvas);
+                
+      
+                Paint p = new Paint();
+                p.setAntiAlias(false);
+                p.setColor(Color.argb(255, 255, 0, 0));
+                
+                if(this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) 
+                {
+                	x_pos = x_InitPos;
+                    y_pos = y_InitPos;
+                    canvas.drawRect(110, 230, 640, 760, p);
+                    //canvas.drawRect(111, 232, 5*(myBmp[0].getWidth()+10), 7*(myBmp[0].getHeight()-1), p);
+                    //d = getResources().getDrawable(R.drawable.start);
+                    //d.setBounds(400,230 ,120, 120);
+                    //canvas.drawRect(110, 770, 610, 850, p);
+                    //p.setColor(Color.argb(0, 255, 0, 0));
+                    //canvas.drawText("shake to start", 120, 780, p);
+                    
+                } 
+                else 
+                    {
+                	x_pos = x_InitPosLand;
+                    y_pos = y_InitPosLand;
+                    canvas.drawRect(350, 40, 880, 570, p);    
+                }	
+                
+                 
+                for (int i = 0; i < 4; i++) {
+        			for (int j = 0; j < 4; j++) {
+        				
+        				d = getResources().getDrawable(mBmpArray[game15puzzle.getFieldNumber(i, j)]);
+        				d.setBounds(x_pos,y_pos ,x_pos+myBmp[0].getWidth()+10, y_pos+myBmp[0].getWidth()+10);
+        				y_pos=y_pos+myBmp[0].getWidth()+10;
+        				d.draw(canvas);
+        				
+        			}
+        			x_pos=x_pos+myBmp[0].getWidth()+10;
+        			if(this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){ 
+        			 y_pos=y_InitPos;
+                    }
+        			else
+        				y_pos=y_InitPosLand;
+        			
+        			d.draw(canvas);
                 }
             }
 
         };
-		
+        
 		setContentView(rootView);
+		rootView.setOnTouchListener(this);
 	}
+}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -58,16 +154,154 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	public void randomizeNumbers()
-	{
-		int tempNum=0,temp;
-		for(int i=0;i<16;i++){
-			if (numbers[i].getRightNumber() == -1)
-				tempNum = numbers[++i].getRightNumber();
-				numbers[i].setRightNumber(tempNum);
-				
-			
+	
+	//set default screen resolution
+	public static void setDefaultScreen(int x, int y) {
+		defaultRes_X = x;
+		defaultRes_Y = y;
+	}
+		//return the current resolution of the screen on Y axis
+	public int getDisplayWidth_X() {
+		//display.getSize(size); 
+		return display.getWidth();
+	}
+
+	//return the current resolution of the screen on Y axis
+	public int getDisplayHeight_Y() {
+		//display.getSize(size); 
+		return display.getHeight();
+	}
+	
+	public int ScreenToField_X(int x) {
+		if(this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){ 
+			x = x - x_InitPos;
+		}
+		else
+			x = x - x_InitPosLand;
+		x = x / myBmp[0].getWidth();
+		return x;
+	}
+
+	public int ScreenToField_Y(int y) {
+		if(this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){ 
+			y = y - y_InitPos;
+		}
+		else
+			y = y - y_InitPosLand;
+		y = y / myBmp[0].getWidth();
+		return y;
+	}
+	
+	//the dialog message by winning the game
+	public void gameOverMsgOnScreen() {
+		 AlertDialog.Builder ad = new AlertDialog.Builder(this);
+		 ad.setTitle("15 Puzzle Game");
+		 ad.setMessage("Congatulations !!!");
+		 ad.setPositiveButton("Play again", new DialogInterface.OnClickListener() {
+
+		  public void onClick(DialogInterface arg0, int arg1) {
+			  // start over the activity when the OK button is clicked
+			  finish();
+			  Intent intent = new Intent(mContext, MainActivity.class);
+	            startActivity(intent); //The method startActivity(Intent)
+		  
+		  }});
+		 ad.setNegativeButton("Quit", new DialogInterface.OnClickListener() {
+		       
+		  public void onClick(DialogInterface arg0, int arg1) {
+			  // close app when the quit button is clicked
+			  finish();
+		  }});
+		 ad.show();
+	}
+
+	
+	@Override
+    public boolean onTouch(View v, MotionEvent event) {
+		 
+		//gameOverMsgOnScreen();
+			if (event.getAction() == MotionEvent.ACTION_DOWN) {
+				if(event.getX() >= x_InitPos && event.getY() <= y_InitPos+(4*myBmp[0].getWidth())){
+					game15puzzle.moveRect(ScreenToField_X((int)event.getX()),ScreenToField_Y((int) event.getY()));
+							v.postInvalidate();
+							if(game15puzzle.IfGameOver()==true)
+							{
+								gameOverMsgOnScreen();
+							}
+				}
+			}
+			return true;
 		}
 
-	}
-}
+	
+	/*
+     * This method will be called whenever the accuracy of the sensor has
+     * changed.
+     */
+   // @Override
+/*    public void onAccuracyChanged(Sensor arg0, int arg1) {
+        // left empty
+    }*/
+
+    /*
+     * This method will be called whenever there are sensor events
+     */
+    //@Override
+    /*public void onSensorChanged(SensorEvent event) {
+    	float x,y,z;
+    	
+    	x=event.values[0];
+        y=event.values[1];
+        z=event.values[2];
+        
+        x10=x10+x;
+        y10=y10+y;
+        z10=z10+z;
+        num = num+1;
+    	
+        if(InitFlag == true && num == 10){
+    		initGyroX = (int)x10;
+    		initGyroY = (int)y10;
+    		initGyroZ = (int)z10;
+    		InitFlag = false;
+    	}
+        if (num == 10){
+        	Log.d(getClass().getName(), String.format("value x = %d", (int)x10));
+        	Log.d(getClass().getName(), String.format("value y = %d", (int)y10));
+        	Log.d(getClass().getName(), String.format("value z = %d", (int)z10));
+        	num=0;
+        	
+        	if(InitFlag == false){
+        		if((int)x10<initGyroX)
+        		{
+        			//slide down once 
+        			int emptyX = game15puzzle.getEmptyX();
+        			int emptyY = game15puzzle.getEmptyY();
+        			game15puzzle.moveRect(emptyX,emptyY-1);
+      
+        			
+        		}
+        		if((int)x10>initGyroX)
+        		{
+        			//slide down once 
+        		}       			
+        	}
+        	
+        	
+        	
+        	
+        }
+        		
+        
+    }*/
+
+    /*
+     * It is good practice to unregister the sensor when not in use
+     */
+    //@Override
+    /*protected void onStop() {
+        sensorManager.unregisterListener(this);
+        super.onStop();
+    }*/
+
+}//end of class
--- FifteenPuzzle/src/com/game/fifteenpuzzle/gameField.java
+++ FifteenPuzzle/src/com/game/fifteenpuzzle/gameField.java
@@ -1,42 +1,82 @@
 package com.game.fifteenpuzzle;
 
+
+
 public class gameField {
+	
 	int field[][] = new int[4][4];	
+	protected int x, y; 
 
 
+//constructor
+public gameField() {
+	CreateNormalField();
+	CreateRandomField();
+}
 
 /*Create Normal Field, the numbers in order 1-15*/	
 private void CreateNormalField()
 {
 	for (int i =0; i<15; i++)
 	{
-		field[i%4][i/4]=i+1;
+		int num =i+1;
+		field[i%4][i/4]= num;
 	}
 }
 
 /*Create Mixed Field, the numbers not in order */
-private void CreateMixedField()
+public void CreateRandomField()
 {
 	int x,y;//position
 	for (int i = 0 ; i<100; i++)
 	{
 		x = (int)(Math.random()*4);
 		y = (int)(Math.random()*4);
-		this.moveNumber(x,y);
+		this.moveRect(x,y);//create random field by moving the rect, so if will be always a solution
 	}
 }
+public int getFieldNumber(int x, int y) {
 
-//constructor
-public gameField() {
-	CreateNormalField();
+	return field[x][y];
+}
+
+public int getEmptyY() {
+
+	int emptyX = -1, emptyY = -1;//empty field coordinates
+	
+	//look for an empty field position
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (this.field[i][j] == 0) {
+					emptyX = i;
+					emptyY = j;
+				}
+			}
+		}
+	return emptyY;
+}
+
+public int getEmptyX() {
+
+	int emptyX = -1, emptyY = -1;//empty field coordinates
+	
+	//look for an empty field position
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (this.field[i][j] == 0) {
+					emptyX = i;
+					emptyY = j;
+				}
+			}
+		}
+	return emptyX;
 }
 
 /*according to the number position on the field move it*/
-public boolean moveNumber(int x, int y) {
-	
-	//empty field coordinates
-	int emptyX=-1,emptyY=-1;
-	boolean result = false;//if there is possibility to move
+public boolean moveRect(int x, int y) {
+
+	int emptyX = -1, emptyY = -1;//empty field coordinates
+	boolean result = false;// result of movement : can move true, can't false
 	
 	//look for an empty field position
 	for (int i = 0; i < 4; i++) {
@@ -47,52 +87,69 @@ public boolean moveNumber(int x, int y) {
 			}
 		}
 	}
-	// check if we cam move
-			if (emptyX == x || emptyY == y) { //same column or same raw
-				if (!(emptyX == x && emptyY == y)) { //the given cell is the empty cell, can't move
-					if (emptyX == x) {// same row
-						if (emptyY < y) { //from the right of the empty cell 
-							for (int i = emptyY+1; i<= emptyY; i++) {
-								field[x][i-1] = field[x][i];//sw
-							}
-						} else {
-							for (int i = emptyY; i > y; i--) {
-								field[x][i] = field[x][i - 1];
-							}
-						}
+	
+	// check if we can move
+	if ( x == emptyX || y == emptyY) {//same column or same raw
+		if (!(x == emptyX && y == emptyY)) { //the given cell is the empty cell, can't move
+			if (x == emptyX) {//same raw with empty field
+				if (emptyY > y) {//from the right of the empty cell 
+					for (int i = emptyY; i > y; i--) {//the number from the left of empty cell
+						field[x][i] = field[x][i - 1];//swap
 					}
-					if (emptyY == y) { //same column
-						if (emptyX < x) { //the number from the right of empty cell
-							for (int i = emptyX+1; i <= x; i++) {
-								field[i-1][y] = field[i][y];
-							}
-						} else {
-							for (int i = emptyX; i > x; i--) {
-								field[i][y] = field[i - 1][y];
-							}
-						}
-					}
-					field[x][y] = 0; // the cell that moved replaced with empty cell position
-					
-					result = true; //we could move the number
-					
-				}// if !(emptyX == x && emptyY == y)
-				
-				else // the empty cell can't move
+				} 
+				else 
 				{
-					result = false;
+					for (int i = emptyY+1; i<= y; i++) {
+						field[x][i-1] = field[x][i];//swap
+					}
 				}
 			}
-	//the result ==> true - can move false==>can't move		
+			if (y == emptyY) {//same column as empty y
+				if (emptyX > x) {//the number from the right of empty cell
+					for (int i = emptyX; i > x; i--) {//the number from the left of empty cell
+						field[i][y] = field[i - 1][y];//swap
+					}
+				} 
+				else 
+				{
+					for (int i = emptyX+1; i <= x; i++) {
+						field[i-1][y] = field[i][y]; //swap
+					}
+				}
+			}
+			
+			field[x][y] = 0;// the cell that moved replaced with empty cell position should be an empty cell
+			result = true; //we could move the number
+		}
+		else // the empty cell can't move
+		{
+			result = false;//we could not move the number
+		}
+	}
+	//return the result
 	return result;
-		
 }	
-	
-	
 
-
-
-
-
+//check the game is over
+public boolean IfGameOver()
+{	
+	int gameOverField[][] = new int[4][4];	
+	int num=0;
+	for (int i =0; i<15; i++)
+	{
+		num =i+1;
+		gameOverField[i%4][i/4]= num;
+	}
+	//check if the normal field is the same as the game field
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if(field[i][j] != gameOverField[i][j])
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
 
 }//end class gameFileld
