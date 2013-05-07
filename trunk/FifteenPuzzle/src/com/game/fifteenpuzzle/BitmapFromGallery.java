89feebc6bc8532c43c1f3cdc830b302d43bb4363
ola.horovits@gmail.com <ola.horovits@gmail.com@914544a8-f3a9-4c03-e07e-afca8276bdf6>
2013-05-07 18:44:29 +0000

    Added the photo capture feature

--- trunk/FifteenPuzzle/src/com/game/fifteenpuzzle/BitmapFromGallery.java
+++ trunk/FifteenPuzzle/src/com/game/fifteenpuzzle/BitmapFromGallery.java
@@ -1,6 +1,9 @@
 package com.game.fifteenpuzzle;
 
+import java.io.File;
+
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -8,6 +11,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -17,8 +21,9 @@ import android.widget.ImageView;
 public class BitmapFromGallery extends Activity{
 	
 private static int RESULT_LOAD_IMAGE = 1;
+private static int RESULT_PHOTO_IMAGE = 2;
 Context mContext;
-
+Uri mCapturedImageURI;
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,7 +59,33 @@ Context mContext;
 				  startActivity(intent);
 				}
 		});
+        
+        Button photoButton = (Button) this.findViewById(R.id.buttonPhoto);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+            	
+            	String fileName = "temp.jpg";  
+                ContentValues values = new ContentValues();  
+                values.put(MediaStore.Images.Media.TITLE, fileName);  
+                mCapturedImageURI = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);  
+
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);  
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCapturedImageURI);  
+                
+                //startActivityForResult(intent, CAPTURE_PICTURE_INTENT);
+            	
+                //Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE); 
+                 
+                //cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                //		MediaStore.Images.Media.EXTERNAL_CONTENT_URI.toString());
+                 
+                startActivityForResult(cameraIntent, RESULT_PHOTO_IMAGE); 
+            }
+        });
     }
+
     
     
     @Override
@@ -76,7 +107,7 @@ Context mContext;
 			
 			//ImageView imageView = (ImageView) findViewById(R.id.imgView);
 			//imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-			bmpPicture = BitmapFactory.decodeFile(picturePath);
+			//bmpPicture = BitmapFactory.decodeFile(picturePath);
 			
 			Intent intent = new Intent(mContext, MainActivity.class);
 			intent.putExtra("type", picturePath);
@@ -84,6 +115,20 @@ Context mContext;
 		
 		}
     
+		if (requestCode == RESULT_PHOTO_IMAGE && resultCode == RESULT_OK ) {  
+			
+			String[] filePathColumn = { MediaStore.Images.Media.DATA}; 
+			Cursor cursor = getContentResolver().query(mCapturedImageURI,
+					filePathColumn, null, null, null);
+			cursor.moveToFirst();
+			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+			String picturePath = cursor.getString(columnIndex);
+			cursor.close();
+            
+            Intent intent = new Intent(mContext, MainActivity.class);
+			intent.putExtra("type", picturePath);
+			startActivity(intent);
+        }  
     
     }
 
